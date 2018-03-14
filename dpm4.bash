1078ac7fbcc9c61e91931be79780064b5df7828a
crookworm <crookworm@users.noreply.github.com>
2018-03-14 17:48:49 -0400

    Delete dpm4.bash

--- dpm4.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-PWR=( 4 4 4 4 4 4 )
-for I in `seq 0 5`; do
-  echo "manual" > /sys/class/drm/card$I/device/power_dpm_force_performance_level
-  echo ${PWR[$I]} > /sys/class/drm/card$I/device/pp_dpm_sclk
-done
